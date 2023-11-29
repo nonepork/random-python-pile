88b540597264d253b241ccff096c43223e98ea85
nonepork <59335048+nonepork@users.noreply.github.com>
2023-11-30 01:34:37 +0800

    aaaaaaaaaaaaaaaa

---
+++ WakeupNeo/matrix.py
@@ -0,0 +1,38 @@
+# idea from https://www.youtube.com/watch?v=gMBqciJJxqc
+from os import system
+from string import ascii_letters, punctuation
+from time import sleep
+
+green_text = "\033[92m"
+
+target = "Wake up, Neo...\nThe Matrix has you...\nFollow the white rabbit.\n\nKnock, Knock, Neo."
+
+ttp = "                                                                                  "
+index = 0
+character_list = ascii_letters + punctuation
+
+for e in target:
+    if(e==' '):
+        index+=1
+        continue
+    elif (e=='\n'):
+        t = list(ttp)
+        t[index] = '\n'
+        ttp = "".join(t)
+        index+=1
+        continue
+    for x in character_list:
+        t = list(ttp)
+        t[index] = x
+        ttp = "".join(t)
+        ttx = f"{green_text}{ttp}"
+        sleep(0.02)
+
+        print("\033c", end="")
+        print(ttx)
+        if(e == x):
+            index+=1
+            break
+
+system("clear")
+system("curl ascii.live/knot")
