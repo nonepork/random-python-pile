a28534ebde1c801706d9fed47d0c63607ccbc20b
nonepork <59335048+nonepork@users.noreply.github.com>
2024-04-05 18:07:11 +0800

    Fix a few stuff

--- IP scanner/README.md
+++ IP scanner/README.md
@@ -1,3 +1,9 @@
 # Ip Scanner
 
 A really simple ip scanner that pings specific ip and check whether respond or not.
+
+### How to use?
+
+Let's say you want to scan from ip 127.0.0.1 to 127.0.255.255.
+
+You simply type in: 127.0.0-255.1-255 and it will start scanning.
--- IP scanner/scan.py
+++ IP scanner/scan.py
@@ -1,16 +1,46 @@
-import os
+import itertools
+import subprocess
 
-def check_ping(ip, start, end):
+def check_ping(ip, *args):
     with open("result.txt", "a") as f:
-        for hostname in range(start, end):
-            response = os.system(f"ping -n 1 {ip}.{hostname}")
+        if len(args) == 0:
+            print(f"[ CHECKING ] {ip}")
             # and then check the response...
-            if response == 0:
-                f.write(f"{ip}.{hostname}\n")
-            else:
-                continue
+            response = subprocess.Popen(f"ping -n 1 {ip}", stdout=subprocess.PIPE)
+            response.wait()
+            if response.poll() == 0:
+                f.write(f"{ip}\n")
+        else:
+            for combination in itertools.product(*args):
+                # combination contains one value from each range
+                temp = ip
+                for i in combination:
+                    temp += "." + str(i)
+                temp.rstrip(".")
 
-ip = str(input("First 3 part of IP:"))
-start = int(input("START:"))
-end = int(input("END:"))
-check_ping(ip, start, end)
+                # and then check the response...
+                print(f"[ CHECKING ] {temp}")
+                response = subprocess.Popen(f"ping -n 1 {temp}", stdout=subprocess.PIP)
+                response.wait()
+                if response.poll() == 0:
+                    f.write(f"{temp}\n")
+                else:
+                    continue
+
+
+def format_input(input: str):
+    unsplit_ip = input.split('.')
+    ip = ""
+    ranges = []
+    for part in unsplit_ip:
+        if "-" not in part:
+            ip += part + "."
+        else:
+            ranges.append(range(int(part.split("-")[0]), int(part.split("-")[1])+1)) # I know this looks scary and I'm sorry xD
+    ip = ip.rstrip(".")
+
+    return ip, ranges
+
+unsplit_ip = str(input("Enter the IP range you want to scan: "))
+ip, ranges = format_input(unsplit_ip)
+check_ping(ip, *ranges)
